905d8f4d08e3814e67feffa898bc9556f455e2cc
Noah Seeger <cuzimnoah1@gmail.com>
2024-07-15 20:21:04 +0200

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import DemoChart from "../components/DemoChart";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
